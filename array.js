a7641ed07795be0a16a119d0c21a00bd19c35abf
maxnawa31 <mnawa31@gmail.com>
2017-11-10 17:48:32 -0800

    adding array exercises

---
+++ array.js
@@ -0,0 +1,28 @@
+Array Basics:
+
+1)var favoriteFoods = ["Sushi", "chips","hot dogs"];
+2)favoriteFoods[1];
+3)favoriteFoods[2]="nachos";
+4)var formerFavoriteFood = favoriteFoods.shift();
+5)favoriteFoods.push("frech fries");
+6)favoriteFoods.unshift("hamburger");
+7)Returns undefined
+8)var array = [2,3,4,5];
+  array.splice(1,1);
+
+
+Array Methods:
+Part 1:
+var arr = [];
+arr.push("Max");
+arr.push("Nawa");
+arr.unshift("orange");
+arr.shift();
+var arr2 = [];
+arr2.push(5);
+arr2.push("Javascript");
+arr2.indexOf(42);
+returns false;
+var combinedArr = arr.concat(arr2);
+
+Part 2:
